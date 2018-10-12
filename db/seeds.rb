d3405ca060ca7458287dfe4f911fe045f21e6a93
Brian Gurtz <bgurtz@gmail.com>
2018-10-12 17:56:42 -0500

    Implemented technolgy field for blogs

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,3 +49,10 @@ end
  end
 
   puts "1 Angular item created"
+
+  3.times do |technology|
+    Portfolio.last.technologies.create!(
+      name: "Technology #{technology}"
+      )
+  end
+  puts "3 technologies created"
