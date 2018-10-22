4112c1c83a3db2eaa5295f66d9becbd9f3afdb22
Brian Gurtz <bgurtz@gmail.com>
2018-10-22 00:21:54 -0500

    Refactored strong params for portfolio items

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module Brian
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    #config.action_controler.permit_all_parameters = true 
+    #The above code above will turn off the white list for the whole site.
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
