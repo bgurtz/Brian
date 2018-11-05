f2198e134b7f78aecd9b2dfcd6907f6478337547
Brian Gurtz <bgurtz@gmail.com>
2018-11-04 21:55:56 -0600

    11/4

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,12 @@
+module CurrentUserConcern
+	extend ActiveSupport::Concern
+	
+	def current_user
+		super || guest = OpenStruct.new(
+							name: "Guest User", 
+							first_name: "Guest", 
+							last_name:"User", 
+							email: "email@example.com"
+							)	
+	end
+end
