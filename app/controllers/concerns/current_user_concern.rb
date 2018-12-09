7e8bacb487c75231c835e6d827a9c762d4baf16b
Brian Gurtz <bgurtz@gmail.com>
2018-12-08 21:03:27 -0600

    Implemented Authorization rolls for users

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -2,11 +2,15 @@ module CurrentUserConcern
 	extend ActiveSupport::Concern
 	
 	def current_user
-		super || guest = OpenStruct.new(
-							name: "Guest User", 
-							first_name: "Guest", 
-							last_name:"User", 
-							email: "email@example.com"
-							)	
+		super || guest_user
+	end
+
+	def guest_user
+		guest = GuestUser.new
+		guest.name = "Guest User"
+		guest.first_name = "Guest"
+		guest.last_name = "User"
+		guest.email = "guest@example.com"
+		guest
 	end
 end
